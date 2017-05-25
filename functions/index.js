9096fe7d8217f15f827cf908e6850b9ad4e9d940
Jack <jacklai5505@gmail.com>
2017-05-25 14:40:45 +0800

    Create function for listening new message in database and send notification to device.

---
+++ functions/index.js
@@ -0,0 +1,38 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+exports.sendMessageNotification = functions.database.ref('/messages/{messageId}').onWrite(event => {
+  const messageId = event.params.messageId;
+  const message = event.data.val();
+
+  admin.database().ref('users/' + message.fromId).once('value', function(snapshot) {
+    const fromUser = snapshot.val();
+
+    admin.database().ref('users/' + message.toId).once('value', function(snapshot) {
+      const toUser = snapshot.val();
+
+      const payload = {
+        notification: {
+          title: fromUser.name,
+          body: message.text,
+          sound: 'default'
+        },
+        data: {
+          user_id: message.fromId,
+          user_name: fromUser.name,
+          user_email: fromUser.email,
+          user_image: fromUser.profileImageUrl
+        }
+      };
+
+      admin.messaging().sendToDevice(toUser.registrationId, payload)
+        .then(function(response) {
+          console.log('Successfully sent message: ', response);
+        })
+        .catch(function(error) {
+          console.log('Error sending message: ', error);
+      });
+    });
+  });
+});
